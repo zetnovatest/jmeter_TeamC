f309d906b077b64f06236b258abf736366ec4211
Zetnova <stoychev.zdravko@gmail.com>
2018-05-02 15:06:44 +0300

    ,

---
+++ RunJMX.ps1
@@ -0,0 +1,10 @@
+﻿$TESTS = Get-ChildItem . | Where-Object {$_.Extension -eq ".jmx"}
+
+Remove-Item -Recurse -Force -ErrorAction 0 @('Results')
+New-Item -ErrorAction Ignore -ItemType directory -Path @('Results')
+
+foreach ($test in $TESTS)
+{
+  Write-Host $test
+  &jmeter  -n -t $test.jmx -l $test.log -e -o Results\$test
+}
